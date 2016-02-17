4675b06c9e15d1c9b2039e7132f9acdae83fd861
mazhen <mazhenhit@foxmail.com>
2016-02-17 21:58:55 +0800

    chg 6.c, accept

--- 6.c
+++ 6.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char *convert(char *s, int numRows)
 {
-	if(NULL == s)
-		return NULL;
+    char *rsl = NULL;
+    int length = 0;
+    int i,j;
+    int count = 0;
+    int index;
 
+    if(NULL == s || numRows <= 1)
+        return s;
+
+    length = strlen(s);
+    rsl = malloc(length + 1);
+    if(rsl == NULL)
+        return NULL;
+
+    for (i = 0; i < numRows; i++)
+    {
+        for (j = 0, index = i; index < length;
+            j++, index = (2 * numRows - 2) * j + i)
+        {
+            rsl[count++] = s[index]; // 垂直元素
+            if (i == 0 || i == numRows - 1)
+                continue; // 斜对角元素
+            if (index + (numRows - i - 1) * 2 < length)
+                rsl[count++] = s[index + (numRows - i - 1) * 2];
+        }
+    }
+    rsl[count] = '\0';
+    return rsl;
 }
 
 int main()
 {
+    printf("%s\n", convert("PAYPALISHIRING", 3));
 	return 0;
 }
-
